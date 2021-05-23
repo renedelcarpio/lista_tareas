2625a76d224fdd401cf5fa8089e5a19dbc8db208
Rene Del Carpio <delcarpio.rene@gmail.com>
2021-05-23 19:41:58 -0400

    se limpiaron archivos innecesarios y se creo el componente Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import React, { useState } from 'react';
 import './App.css';
+import FormToDo from './components/FormToDo';
 import Header from './components/Header'
+import TodoList from './components/TodoList';
 
 const App = () => {
+  const [tareas, setTareas] = useState(
+    [
+      {
+        id: 1,
+        text: 'Lavar la ropa',
+        complete: false
+      },
+      {
+        id: 2,
+        text: 'Grabar turorial',
+        complete: false
+      }
+    ]
+  )
   return (
     <div className='contenedor'>
-      <Header></Header>
+      <Header/>
+      <FormToDo tareas={tareas} setTodo={setTareas} />
+      <TodoList tareas={tareas}/>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
     return (
         <header className='header'>
-            <h1 className='header__title'>Lista de Tareas</h1>
-            <button className='header__button'>
+            <h1 className='header__titulo'>Lista de Tareas</h1>
+            <button className='header__boton'>
                 No mostrar completadas
+                <FontAwesomeIcon icon={faEyeSlash} className='header__icon-button'/>
             </button>
         </header>
     )

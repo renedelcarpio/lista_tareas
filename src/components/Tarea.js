6748cf6651344db643cf8c6f742146af76ca1f97
Rene Del Carpio <delcarpio.rene@gmail.com>
2021-05-23 22:49:20 -0400

    se agrego funcionalidad al formulario de la lista de tareas

--- src/components/Tarea.js
+++ src/components/Tarea.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
 	faCheckSquare,
@@ -7,17 +7,46 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 
 const Tarea = ({ tarea }) => {
+	const [editandoTarea, cambiarEditandoTarea] = useState(false);
+	const [nuevaTarea, cambiarNuevaTarea] = useState(tarea.texto);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		cambiarEditandoTarea(false);
+	};
+
 	return (
 		<li className='lista-tareas__tarea'>
 			<FontAwesomeIcon
 				icon={faCheckSquare}
 				className='lista-tareas__icono lista-tareas__icono-check'
 			/>
-			<div className='lista-tareas__texto'>{tarea.texto}</div>
+			<div className='lista-tareas__texto'>
+				{editandoTarea ? (
+					<form
+						action=''
+						className='formulario-editar-tarea'
+						onSubmit={handleSubmit}
+					>
+						<input
+							type='text'
+							className='formulario-editar-tarea__input'
+							value={nuevaTarea}
+							onChange={(e) => cambiarNuevaTarea(e.target.value)}
+						/>
+						<button type='submit' className='formulario-editar-tarea__btn'>
+							Actualizar
+						</button>
+					</form>
+				) : (
+					tarea.texto
+				)}
+			</div>
 			<div className='lista-tareas___contenedor-botones'>
 				<FontAwesomeIcon
 					icon={faEdit}
 					className='lista-tareas__icono lista-tareas__icono-accion'
+					onClick={() => cambiarEditandoTarea(!editandoTarea)}
 				/>
 				<FontAwesomeIcon
 					icon={faTimes}
